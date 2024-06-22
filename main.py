817ab0d99239c8f2efb03e076b61759cb8514f01
10_sanyam_01 <144155690+SanyamRajput@users.noreply.github.com>
2024-06-22 12:41:58 +0530

    added ball class

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+import time
 
 scr = Screen()
 scr.setup(800,600)
@@ -10,6 +12,8 @@ scr.tracer(0)
 r_paddle = Paddle((350,0))
 l_paddle = Paddle((-350,0))
 
+ball = Ball()
+
 scr.listen()
 scr.onkey(r_paddle.paddle_up,'Up')
 scr.onkey(r_paddle.paddle_down,'Down')
@@ -17,10 +21,11 @@ scr.onkey(l_paddle.paddle_up,'w')
 scr.onkey(l_paddle.paddle_down,'s')
 
 
-
 ongame = True
 while ongame:
+    time.sleep(0.1)
     scr.update()
+    ball.move()
 
 
 scr.exitonclick()
