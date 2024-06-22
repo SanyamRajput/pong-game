454063c1196d23185e65b7de326ecd8bc5c149de
10_sanyam_01 <144155690+SanyamRajput@users.noreply.github.com>
2024-06-22 11:35:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ scr.setup(800,600)
 scr.bgcolor('black')
 scr.title('Pong Game')
 
+paddle = Turtle() 
+paddle.color('white')
+paddle.shape('square')
+paddle.shapesize(stretch_wid=5, stretch_len=1)        
+paddle.goto(350,0)       
+paddle.listen()
+
 
 
 
