5a8cb38d4055ff9df2798acc03578d416f7b8abf
10_sanyam_01 <144155690+SanyamRajput@users.noreply.github.com>
2024-06-22 12:01:04 +0530

    Create paddle.py
    added paddle module

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
-from turtle import Turtle,Screen
+from turtle import Screen
+from paddle import Paddle
 
 scr = Screen()
 scr.setup(800,600)
 scr.bgcolor('black')
 scr.title('Pong Game')
+scr.tracer(0)
 
-paddle = Turtle() 
-paddle.color('white')
-paddle.shape('square')
-paddle.shapesize(stretch_wid=5, stretch_len=1)        
-paddle.goto(350,0)       
-paddle.listen()
+r_paddle = Paddle(350)
+l_paddle = Paddle(-350)
+
+scr.listen()
+scr.onkey(r_paddle.paddle_up,'Up')
+scr.onkey(r_paddle.paddle_down,'Down')
+scr.onkey(l_paddle.paddle_up,'Up')
+scr.onkey(l_paddle.paddle_down,'Down')
 
 
+
+ongame = True
+while ongame:
+    scr.update()
 
 
 scr.exitonclick()
