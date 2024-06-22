18a54f19e051b636e172fe6c2c433f870a973b5d
10_sanyam_01 <144155690+SanyamRajput@users.noreply.github.com>
2024-06-22 12:59:59 +0530

    changes

--- paddle.py
+++ paddle.py
@@ -1,20 +1,21 @@
 from turtle import Turtle, Screen
 
 class Paddle(Turtle):
-    def __init__(self,position):
+    def __init__(self, position):
         super().__init__()
-        self.penup()
         self.shape('square')
         self.color('white')
         self.shapesize(stretch_wid=5, stretch_len=1)
-        self.goto(position,0)
+        self.penup()
+        self.goto(position)
 
     def paddle_up(self):
         y = self.ycor() + 20
-        self.goto(0, y)
+        self.goto(self.xcor(), y)
 
     def paddle_down(self):
         y = self.ycor() - 20
-        self.goto(0, y)
+        self.goto(self.xcor(), y)
+
 
 
